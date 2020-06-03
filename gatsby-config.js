3e0692b596f997274bf3b65d6d7e47cbfb44a905
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-06-03 10:52:23 +0200

    task: configure gatsby-source-prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,33 @@
+
+// Can be configured in gatsby-cloud
+process.env.PRISMIC_REPO_NAME = process.env.PRISMIC_REPO_NAME || "gatsby-starter-default";
+
+process.env.PRISMIC_API_KEY = process.env.PRISMIC_API_KEY || undefined;
+
+// Release to preview 
+process.env.PRISMIC_RELEASE_ID = process.env.PRISMIC_RELEASE_ID || "XtdZ4BIAACMANi0x";
+
+// now to set up gatsby-cloud and then a preview
+// For redirecting from prismic to the page on gatsby-preview
+// process.env.PRISMIC_PREVIEW_PATH = process.env.PRISMIC_PREVIEW_PATH || "/previews";
+// 
+
+
+const linkResolver = require('./src/prismic/linkResolver');
+
+const gastbySourcePrismicConfig = {
+  resolve: 'gatsby-source-prismic',
+  options: {
+    repositoryName: process.env.PRISMIC_REPO_NAME,
+    linkResolver,
+    schemas: {
+      // Custom types mapped to schemas
+      homepage: require("./src/schemas/homepage.json"),
+      page: require('./src/schemas/page.json')
+    },
+  }
+};
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -30,5 +60,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    gastbySourcePrismicConfig
   ],
 }
