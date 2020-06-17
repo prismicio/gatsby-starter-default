42bf5b6d40800879565d448b7a532511a54e566a
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-06-17 15:37:22 +0200

    Support previews during ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react'
+import { PreviewStoreProvider } from 'gatsby-source-prismic'
+
+export const wrapRootElement = ({ element }) => (
+  <PreviewStoreProvider>{element}</PreviewStoreProvider>
+)
